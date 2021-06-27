ac51c99621048f3a88429bb0e6af145d3be39ecf
Brandon Yao <ahoynodnarb@gmail.com>
2021-06-26 21:43:17 -0700

    Added retrying and highscores

--- main.py
+++ main.py
@@ -10,54 +10,24 @@ class Snake:
     def __init__(self, size=10, speed=0.3, initial_size=4):
         self.valid_inputs = {curses.KEY_UP: (
             0, -1), curses.KEY_DOWN: (0, 1), curses.KEY_RIGHT: (1, 1), curses.KEY_LEFT: (1, -1)}
+        self.__target = '🍌'
+        self.__initial_size = initial_size
         self.__board_size = size
-        self.__board = [['  ']*size for _ in range(size)]
-        self.__body_coords = [[0, i] for i in range(initial_size)]
-        free_spaces = self._generate_free_coords()
-        self._apple_coords = free_spaces[randint(0, len(free_spaces)-1)]
-        self.__board[self._apple_coords[0]][self._apple_coords[1]] = '🍎'
+        self.__speed = speed
+        self.__score = 0
+        self.__highscores = []
+        self.initialize()
+
+    def initialize(self):
+        self.__board = [
+            ['  ']*self.__board_size for _ in range(self.__board_size)]
+        self.__body_coords = [[0, i] for i in range(self.__initial_size)]
+        self._add_target()
         self.__direction = [0, 1]
         # the first element signifies whether it's an x or y translation 0: y 1: x
         # the second element signifies direction 1: right -1: left
-        self.__speed = speed
         self.__score = 0
         self.alive = True
-
-    def _generate_free_coords(self):
-        return [(i, x) for i in range(self.__board_size)
-                for x in range(len(self.__board[i])) if([i, x] not in self.__body_coords)]
-
-    def _update_state(self):
-        head = self.__body_coords[0]
-        tail = self.__body_coords[1:]
-        self.alive = head not in tail
-
-    def set__direction(self, key):
-        idx = self.valid_inputs[key]
-        if(idx[0] == self.__direction[0] and idx[1] == -self.__direction[1]):
-            return
-        self.__direction[0] = idx[0]
-        self.__direction[1] = idx[1]
-
-    def _clear__board(self):
-        for i in range(self.__board_size):
-            for x in range(len(self.__board[i])):
-                if(self.__board[i][x] != '🍎'):
-                    self.__board[i][x] = '  '
-        for j in self.__body_coords:
-            if(self.__body_coords.index(j) == 0):
-                self.__board[j[0]][j[1]] = '█ '
-                continue
-            self.__board[j[0]][j[1]] = 'O '
-
-    def _level_up(self):
-        new_tail = deepcopy(self.__body_coords)[-1]
-        new_tail[self.__direction[0]] -= self.__direction[1]
-        self.__body_coords.append(new_tail)
-        free_spaces = self._generate_free_coords()
-        self._apple_coords = free_spaces[randint(0, len(free_spaces)-1)]
-        self.__board[self._apple_coords[0]][self._apple_coords[1]] = '🍎'
-        self.__score += 1
 
     def move(self):
         new_head = deepcopy(self.__body_coords)[0]
@@ -92,10 +62,56 @@ class Snake:
         sleep(self.__speed)
 
     def end(self):
+        self.__highscores.append(self.__score)
+        self.__highscores = sorted(list(set(self.__highscores)), reverse=True)
+        self.__highscores = self.__highscores[:3]
         self.__board = [
             ['  ']*self.__board_size for _ in range(self.__board_size)]
         self.__board[3][3] = 'You lose'
         self.__board[4][3] = 'Score = '+str(self.__score)
+        for i in range(len(self.__highscores)):
+            self.__board[i+5][3] = f'{i+1}: {self.__highscores[i]}'
+        self.__board[9][2] = 'Play again? y/n'
+
+    def set_direction(self, key):
+        idx = self.valid_inputs[key]
+        if(idx[0] == self.__direction[0] and idx[1] == -self.__direction[1]):
+            return
+        self.__direction[0] = idx[0]
+        self.__direction[1] = idx[1]
+
+    def _add_target(self):
+        free_spaces = self._generate_free_coords()
+        self._apple_coords = free_spaces[randint(0, len(free_spaces)-1)]
+        self.__board[self._apple_coords[0]
+                     ][self._apple_coords[1]] = self.__target
+
+    def _clear__board(self):
+        for i in range(self.__board_size):
+            for x in range(len(self.__board[i])):
+                if(self.__board[i][x] != self.__target):
+                    self.__board[i][x] = '  '
+        for j in self.__body_coords:
+            if(self.__body_coords.index(j) == 0):
+                self.__board[j[0]][j[1]] = '█ '
+                continue
+            self.__board[j[0]][j[1]] = 'O '
+
+    def _generate_free_coords(self):
+        return [(i, x) for i in range(self.__board_size)
+                for x in range(len(self.__board[i])) if([i, x] not in self.__body_coords)]
+
+    def _level_up(self):
+        new_tail = deepcopy(self.__body_coords)[-1]
+        new_tail[self.__direction[0]] -= self.__direction[1]
+        self.__body_coords.append(new_tail)
+        self._add_target()
+        self.__score += 1
+
+    def _update_state(self):
+        head = self.__body_coords[0]
+        tail = self.__body_coords[1:]
+        self.alive = head not in tail
 
 
 def main(stdscr):
@@ -106,9 +122,13 @@ def main(stdscr):
     while True:
         key = stdscr.getch()
         if(key in snake.valid_inputs):
-            snake.set__direction(key)
+            snake.set_direction(key)
         if(not snake.alive):
             snake.end()
+            if(key == 121):
+                snake.initialize()
+            elif(key == 110):
+                exit()
         else:
             snake.move()
         snake.redraw(stdscr)
